bdd24b7083836135145c6ced03b3992a3324d282
akshaytanmane150294 <akshaytanmane1@gmail.com>
2024-08-12 21:20:14 -0400

    modified files

--- bash.sh
+++ bash.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # This installs the CodeDeploy agent and its prerequisites on Ubuntu 22.04. 
-sudo apt-get update
-sudo apt-get install ruby-full ruby-webrick wget -y
-cd /tmp
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb
-mkdir codedeploy-agent_1.3.2-1902_ubuntu22
-dpkg-deb -R codedeploy-agent_1.3.2-1902_all.deb codedeploy-agent_1.3.2-1902_ubuntu22
-sed 's/Depends:.*/Depends:ruby3.0/' -i ./codedeploy-agent_1.3.2-1902_ubuntu22/DEBIAN/control
-dpkg-deb -b codedeploy-agent_1.3.2-1902_ubuntu22/
-sudo dpkg -i codedeploy-agent_1.3.2-1902_ubuntu22.deb
-systemctl list-units --type=service | grep codedeploy
-sudo service codedeploy-agent status
-
-
-# sudo apt --fix-broken install
 # sudo apt-get update
-# sudo apt-get install -y ruby3.0
-# cd /home/ubuntu
-# wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-# chmod +x ./install
-# sudo ./install auto
-# sudo service codedeploy-agent start
-# sudo systemctl status codedeploy-agent
+# sudo apt-get install ruby-full ruby-webrick wget -y
+# cd /tmp
+# wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb
+# mkdir codedeploy-agent_1.3.2-1902_ubuntu22
+# dpkg-deb -R codedeploy-agent_1.3.2-1902_all.deb codedeploy-agent_1.3.2-1902_ubuntu22
+# sed 's/Depends:.*/Depends:ruby3.0/' -i ./codedeploy-agent_1.3.2-1902_ubuntu22/DEBIAN/control
+# dpkg-deb -b codedeploy-agent_1.3.2-1902_ubuntu22/
+# sudo dpkg -i codedeploy-agent_1.3.2-1902_ubuntu22.deb
+# systemctl list-units --type=service | grep codedeploy
+# sudo service codedeploy-agent status
+
+
+sudo apt --fix-broken install
+sudo apt-get update
+sudo apt-get install -y ruby3.0
+cd /home/ubuntu
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
+sudo systemctl status codedeploy-agent
